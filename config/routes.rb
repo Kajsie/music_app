ee2da2d89f58104c4e5ea935b40411b2b22a3970
kajsa kristersson <k.kristersson@gmail.com>
2017-11-27 17:27:52 +0100

    routes added to playlists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :playlists
+
   root 'welcome#index' # tells Rails to map requests to the root of the application to the welcome controller's index action
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
