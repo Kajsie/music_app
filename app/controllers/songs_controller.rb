2cf07f0f74e27dd4c335388650091e14ae7bfd0c
kajsa kristersson <k.kristersson@gmail.com>
2017-12-01 00:24:02 +0100

    delete method added for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,13 @@ class SongsController < ApplicationController
     redirect_to playlist_path(@playlist)
   end
 
+  def destroy
+    @playlist = Playlist.find(params[:playlist_id])
+    @song = @playlist.songs.find(params[:id])
+    @song.destroy
+    redirect_to playlist_path(@playlist)
+  end
+
   private
     def song_params
       params.require(:song).permit(:name, :artist, :link, :votes)
