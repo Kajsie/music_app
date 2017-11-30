45f935e737be0249af64d1413accc6e8e501ec09
kajsa kristersson <k.kristersson@gmail.com>
2017-11-30 16:57:15 +0100

    nested routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :playlists # resources is a method, allows the routing to all CRUD actions in the playlists controllers
-
+  resources :playlists do # resources is a method, allows the routing to all CRUD actions in the playlists controllers
+    resources :songs # creates nested resources within playlists
+  end
   root 'welcome#index' # tells Rails to map requests to the root of the application to the welcome controller's index action
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
