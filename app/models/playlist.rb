e5abd713c73c17013cbc20f75998e9c54b3a10dc
kajsa kristersson <k.kristersson@gmail.com>
2017-12-01 00:32:21 +0100

    dependent destroy added

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,7 @@
 class Playlist < ApplicationRecord
-  has_many :songs
+  has_many :songs, dependent: :destroy # NOTE that this dependecy might
+  # have to be taken away if the same songs are used for different playlists
   validates :title, presence: true, length: { minimum: 3 }
 end
+
+
