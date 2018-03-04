9f2815df16ad6edc02cc90429e5b3693a9decf71
kajsa kristersson <k.kristersson@gmail.com>
2018-03-04 16:02:40 +0100

    changed login to be after welcome

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,5 @@ module MusicApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    RSpotify::authenticate(ENV['spotify_id'], ENV['spotify_secret'])
   end
 end
